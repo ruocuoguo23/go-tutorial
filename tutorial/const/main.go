4acdc7814eee59f505b0fc7a2954b789c1d560a6
ruocuoguo <ruocuoguo@163.com>
2024-06-07 16:41:47 +0800

    feat: add const tutorial

---
+++ tutorial/const/main.go
@@ -0,0 +1,17 @@
+package main
+
+type Weekday int
+
+const (
+	Sunday    Weekday = iota // 0
+	Monday                   // 1
+	Tuesday                  // 2
+	Wednesday                // 3
+	Thursday                 // 4
+	Friday                   // 5
+	Saturday                 // 6
+)
+
+func main() {
+	println(Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday)
+}
